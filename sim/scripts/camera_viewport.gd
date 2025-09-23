906df151f71de6d454fbdc75b3a66ea5ef7f8e2d
Leonid Briskindov <lb357@yandex.ru>
2025-09-23 20:46:54 +0300

    update cams

--- sim/scripts/camera_viewport.gd
+++ sim/scripts/camera_viewport.gd
@@ -1,11 +1,8 @@
 extends SubViewport
 
 @export var target: Node3D
-@export var target_position_offset: Vector3
-@export var target_rotation_offset: Vector3
 @onready var camera: Camera3D = $Camera
 
 
 func _process(delta: float) -> void:
-	camera.global_position = target.global_position + target_position_offset
-	camera.global_rotation_degrees = target.global_rotation_degrees + target_rotation_offset
+	camera.global_transform = target.global_transform
